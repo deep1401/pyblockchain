80c851452bd3a738267dcedc5a711e00009b7c1d
deep1401 <gandhi0869@gmail.com>
2020-02-21 15:45:21 +0530

    CORS

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import datetime
 import hashlib
 import json
 import pytz
+from flask_cors import CORS,cross_origin
 
 
 class Blockchain:
@@ -25,14 +26,14 @@ class Blockchain:
         return self.chain[-1]
 
     def proof_of_work(self,previous_proof):
-        new_proof=1
-        check_proof= False
+        new_proof = 1
+        check_proof = False
         while check_proof is False:
-            hash_op=hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_op = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_op[:4]=='0000':
                 check_proof=True
             else:
-                new_proof+=1
+                new_proof += 1
         return new_proof
 
     def hash(self,block):
@@ -41,22 +42,23 @@ class Blockchain:
 
     def chain_validity(self,chain):
         previous_block=chain[0]
-        block_index=1
+        block_index = 1
         while block_index<len(chain):
             block=chain[block_index]
-            if block['previous_hash']!=self.hash(previous_block):
+            if block['previous_hash'] != self.hash(previous_block):
                 return False
-            previous_proof=previous_block['proof']
+            previous_proof = previous_block['proof']
             proof=block['proof']
             hash_op=hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_op[:4]=='0000':
+            if hash_op[:4] == '0000':
                 return False
             previous_block=block
-            block_index+=1
+            block_index += 1
         return True
 
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 blockchain = Blockchain()
 
